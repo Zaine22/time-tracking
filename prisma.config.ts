4210ad1e65968d8a32da26e1a5458dfea7d87a14
Zaine <kyawkokozin223@gmail.com>
2026-05-26 16:08:07 +0630

    fix : for production

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,8 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "Prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DATABASE_URL || process.env.POSTGRES_URL || process.env.PRISMA_DATABASE_URL,
   },
 });
